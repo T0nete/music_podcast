8498389ea8ab95201d601750e20e01ae1d0baf24
T0nete <tonbarramon@gmail.com>
2023-06-19 23:55:13 +0200

    header test

---
+++ src/components/Header.test.js
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react'
+import { Provider, useDispatch } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { store } from '../store/index'
+import '@testing-library/jest-dom/extend-expect'
+import Header from './Header';
+import { configureStore } from '@reduxjs/toolkit';
+import { setLoadingState } from '../store/loadingSlice'
+import loadingReducer from '../store/loadingSlice'
+
+describe('Header', () => {
+  test('renders Header component', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Header />
+        </Router>
+      </Provider>
+    );
+
+    const titleElement = screen.getByText('Podcaster');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('renders Header component with spinner', () => {
+    // Set loading state to true
+    const testStore = configureStore({
+        reducer: { loading: loadingReducer },
+    })
+    testStore.dispatch(setLoadingState(true))
+
+    render(
+        <Provider store={testStore}>
+            <Router>
+                <Header />
+            </Router>
+        </Provider>
+    )
+
+      const spinnerElement = screen.queryByRole('status');
+      expect(spinnerElement).toBeInTheDocument();
+  })
+
+  test('renders Header component without spinner', () => {
+    // Set loading state to false
+    const testStore = configureStore({
+        reducer: { loading: loadingReducer },
+    })
+
+    testStore.dispatch(setLoadingState(false))
+
+    render(
+      <Provider store={testStore}>
+        <Router>
+          <Header />
+        </Router>
+      </Provider>
+    )
+
+    const spinnerElement = screen.queryByRole('status')
+    expect(spinnerElement).toBeNull()
+  })
+});
